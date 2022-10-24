d47df58bac3dc5bace38d11834c09a197b7041c6
uzziel <aliafifmm@gmail.com>
2022-10-25 00:44:01 +0330

    read 'flask secret' as env var

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from os import environ
+
+SECRET = environ.get("APP_SECRET_KEY")
+
+if not SECRET:
+    print("env 'APP_SECRET_KEY' not found")
+    exit(1)
 
 app = Flask(__name__)
-app.secret_key = "secret key"
+app.secret_key = SECRET
